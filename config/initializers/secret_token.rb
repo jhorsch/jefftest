c381ff5499cb25e15252f4548d8ed183902eedeb
Jeff Horsch <jhorsch@gmail.com>
2014-02-04 11:47:14 -0600

    firstcommit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jefftest::Application.config.secret_key_base = 'fce46d9432dc6df6ad41d46a9f58b12a650a20e2a8924ac30db305e4b79fa931825780aa8a0df7872927fca2676d7d2fc7ea3402b79234afa645c0da8011f838'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Jefftest::Application.config.session_store :cookie_store, key: '_jefftest_session'
